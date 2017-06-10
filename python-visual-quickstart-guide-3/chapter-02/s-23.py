6e1d8aa045f371ef3db24437bd49be0dc4307bfc
leexiaosi <leexiaosi@gmail.com>
2017-06-10 21:26:47 +0800

    提交第二章笔记

--- python-visual-quickstart-guide-3/chapter-02/s-23.py
+++ python-visual-quickstart-guide-3/chapter-02/s-23.py
@@ -6,27 +6,40 @@ class S23Test(unittest.TestCase):
     # 加法
     def test_add(self):
         value = 3.5 + 42.7
-        self.assertLess(value, 100)
+        self.assertEqual(value, 46.2)
 
     # 减法
     def test_sub(self):
         value = 3.5 - 42.7
-        self.assertLess(value, 1)
+        self.assertEqual(value, -39.2)
 
     # 乘法
     def test_multi(self):
         value = 3.5 * 42
-        self.assertLess(value, 200)
+        self.assertEqual(value, 147.0)
 
     # 除法
-    def test_divi(self):
+    def test_division(self):
         value = 42.7 / 34.8
         self.assertGreater(value, 1)
 
     # 科学记数
-    def test_kexue(self):
+    def test_scientific_notation(self):
         value = 2.30e2
         self.assertEqual(value, 230.0)
+
+    # 整除
+    def test_divide_exactly(self):
+        value = 42.3 // 4
+        self.assertEqual(value, 10.0)
+
+    def test_test_remainder(self):
+        value = 42.0 % 4
+        self.assertEqual(value, 2.0)
+
+    def test_complex_number(self):
+        value = 1j * 1j
+        self.assertEqual(value, -1)
 
 
 if __name__ == "__main__":
